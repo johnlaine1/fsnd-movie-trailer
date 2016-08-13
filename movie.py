b2cd1d42c162691ee5e610bf5d001fa76b6d3e88
John Laine <john.laine5896@gmail.com>
2016-08-13 01:33:55 +0000

    docs: Add comments to movie class

--- movie.py
+++ movie.py
@@ -1,13 +1,16 @@
-import webbrowser
-
 class Movie():
-    ''' A class used to store movie related information '''
+    ''' A class used to store movie related information .
+    
+    Attributes:
+        title (str): The title of the movie.
+        storyline (str): A brief description of the movie.
+        poster_image (str): A URL to an image file of poster for the movie.
+        trailer_youtube_url: A URL to a youtube video for the movie.
+        
+    '''
     def __init__(self, title, storyline, poster_image, 
                  trailer):
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer
-        
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
