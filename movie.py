7f668357a9a7548d1475f53f4b643b3fff013549
John Laine <john.laine5896@gmail.com>
2016-08-13 03:12:52 +0000

    just started, got sidetracked

--- movie.py
+++ movie.py
@@ -9,11 +9,15 @@ class Movie():
         
     '''
     def __init__(self, title, storyline, poster_image, 
-                 trailer):
+                 trailer, template):
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer
+        self.template = template
         
     def render_movie_tile(self):
+        content = ''
         
+        
+        return content
