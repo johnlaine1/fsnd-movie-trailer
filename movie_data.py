e331f10b70bec46351e068d4d37f58cd12f041c6
John Laine <john.laine5896@gmail.com>
2016-08-15 22:54:41 +0000

    feature: Use 'The Movie Database' to get movie content via thier API
    Issue #5
    Closses #5

--- movie_data.py
+++ movie_data.py
@@ -1,53 +1,48 @@
+import requests
 import movie
 from mtw_utils import get_template
 
-despicable_me = movie.Movie(
-    'Despicable Me',
-    'Gru tries to take over the world',
-    'https://upload.wikimedia.org/wikipedia/en/d/db/Despicable_Me_Poster.jpg',
-    'https://www.youtube.com/watch?v=fNPcZWsTgf0',
-    get_template('templates/movie_tile_content.html'),
-    'children'
-    )
-the_goonies = movie.Movie(
-    'The Goonies',
-    'In order to save their home from foreclosure, a group of misfits set out to find a pirate\'s ancient valuable treasure.',
-    'https://upload.wikimedia.org/wikipedia/en/c/c6/The_Goonies.jpg',
-    'https://www.youtube.com/watch?v=5qA2s_Vh0uE',
-    get_template('templates/movie_tile_content.html'),
-    'action'
-    )    
-love_actually = movie.Movie(
-    'Love Actually',
-    'Follows the lives of eight very different couples in dealing with their love lives in various loosely interrelated tales all set during a frantic month before Christmas in London, England.',
-    'https://upload.wikimedia.org/wikipedia/en/e/eb/Love_Actually_movie.jpg',
-    'https://www.youtube.com/watch?v=cYCkFTyADJ0',
-    get_template('templates/movie_tile_content.html'),
-    'romantic-comedy'
-    )
-tangled = movie.Movie(
-    'Tangled',
-    'The magically long-haired Rapunzel has spent her entire life in a tower, but now that a runaway thief has stumbled upon her, she is about to discover the world for the first time, and who she really is.',
-    'https://upload.wikimedia.org/wikipedia/en/a/a8/Tangled_poster.jpg',
-    'https://www.youtube.com/watch?v=ip_0CFKTO9E',
-    get_template('templates/movie_tile_content.html'),
-    'children'
-    )
-pulp_fiction = movie.Movie(
-    'Pulp Fiction',
-    'The lives of two mob hit men, a boxer, a gangster\'s wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
-    'https://upload.wikimedia.org/wikipedia/en/8/82/Pulp_Fiction_cover.jpg',
-    'https://www.youtube.com/watch?v=s7EdQ4FqbhY',
-    get_template('templates/movie_tile_content.html'),
-    'action'
-    )
-the_matrix = movie.Movie(
-    'The Matrix',
-    'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.',
-    'https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg',
-    'https://www.youtube.com/watch?v=m8e-FF8MsqU',
-    get_template('templates/movie_tile_content.html'),
-    'science-fiction'
-    )
-                            
-movies = [despicable_me, the_goonies, love_actually, tangled, pulp_fiction, the_matrix]
+def get_movie_data(movie_id):
+    API_KEY = '9c23d458160dc620ec8a017d584552f5'
+    req_params = {'api_key': API_KEY}
+    
+    # fetch the movie data
+    movie_req = requests.get('https://api.themoviedb.org/3/movie/' + movie_id, params=req_params)
+    movie_res = movie_req.json()
+    
+    # Fetch the trailer data, unfortunately the trailer url is not available 
+    # in the movie request object.
+    trailer_req = requests.get('http://api.themoviedb.org/3/movie/' + movie_id + '/videos', params=req_params)
+    trailer_res = trailer_req.json()
+    trailer_url = trailer_res['results'][0]['key']
+    
+    return {'movie_info': movie_res, 'trailer_url': trailer_url}
+
+
+def create_movie(movie_id):
+    movie_data = get_movie_data(movie_id)
+    movie_info = movie_data['movie_info']
+    trailer_url = movie_data['trailer_url']
+    
+    movie_obj = movie.Movie(
+        movie_info['title'],
+        movie_info['overview'],
+        'https://image.tmdb.org/t/p/original' + movie_info['poster_path'],
+        'https://youtu.be/' + trailer_url,
+        get_template('templates/movie_tile_content.html'),
+        movie_info['genres'][0]['name'].lower()
+        )
+
+    return movie_obj
+    
+def get_movies(movie_ids):
+    movies = []
+    for movie_id in movie_ids:
+        movie = create_movie((movie_id))
+        movies.append(movie)
+    return movies
+
+
+movie_ids = ['20352', '9340', '508', '38757', '680', '603']
+
+movies = get_movies(movie_ids)
